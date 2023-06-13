c5724fed4d3534bef8c4438a969f9b43fbfd5106
xReLeo <xcoderx00@gmail.com>
2023-06-13 20:26:22 +0200

    now no registration is needed

--- index.js
+++ index.js
@@ -40,43 +40,12 @@ router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/home/index.html'));
 });
 
-router.get('/register', function (req, res) {
-  res.sendFile(path.join(__dirname + '/public/register/register.html'));
-});
-
 app.use('/', router);
 app.use(express.json());//indispensabile sennò il body della request è undefinied
 app.use(express.text());
 
 //you can perform a post request to each route of the server, specifing it
 //NB: you can also do it to a route which does not already exist: in this case that route is created
-app.post('/register', (request, response) => {
-
-  //find username in the database:
-  //if it does already exist gives an error and asks to change username
-  //otherwhise it inserts user and password in the database
-  const data = request.body;
-  const user = data.user;
-  // const pass = data.pass;
-
-  db.find({ user: user }, (err, docs) => {
-    if (docs.length == 0) {
-      db.insert(data);//to insert data in the database
-      console.log(data);
-      response.json({
-        status: 'success',
-        user: user,
-        // pass: pass
-      })
-    }
-    else {
-      console.log('user already existing');
-      response.json({
-        status: 'user already existing',
-      })
-    }
-  });
-})
 
 app.post('/access', (request, response) => {
 
@@ -86,180 +55,61 @@ app.post('/access', (request, response) => {
   const data = request.body;
   const user = data.user;
 
-  db.find({ user: user }, (err, docs) => {
-    if (docs.length != 0) {
-      let user_data = docs[0];//data in database about the user logging in
-      console.log(user_data);
-
-      router.get(`/LogicGames/${user}`, (req, res) => {
-        res.sendFile(__dirname + '/LogicGames/index.html');
-      });
-
-      //skyscrapers
-      router.get(`/LogicGames/${user}/SkyScrapers`, (req, res) => {
-        res.sendFile(path_to_skyscrapers + '/index.html');
-      })
-
-      router.get(`/LogicGames/${user}/SkyScrapers/Clues`, (req, res) => {
-        res.sendFile(path_to_skyscrapers + '/Clues.js');
-      })
-      router.get(`/LogicGames/${user}/SkyScrapers/Grid`, (req, res) => {
-        res.sendFile(path_to_skyscrapers + '/Grid.js');
-      })
-      router.get(`/LogicGames/${user}/SkyScrapers/GameCreation`, (req, res) => {
-        res.sendFile(path_to_skyscrapers + '/GameCreation.js');
-      })
-      router.get(`/LogicGames/${user}/SkyScrapers/Interaction`, (req, res) => {
-        res.sendFile(path_to_skyscrapers + '/Interaction.js');
-      })
-      
-      //battleship
-      router.get(`/LogicGames/${user}/BattleShip`, (req, res) => {
-        res.sendFile(path_to_BattleShip + '/index.html');
-      })
-
-      router.get(`/LogicGames/${user}/BattleShip/Elements`, (req, res) => {
-        res.sendFile(path_to_BattleShip + '/Elements.js');
-      })
-      router.get(`/LogicGames/${user}/BattleShip/Grid`, (req, res) => {
-        res.sendFile(path_to_BattleShip + '/Grid.js');
-      })
-      router.get(`/LogicGames/${user}/BattleShip/Indizi`, (req, res) => {
-        res.sendFile(path_to_BattleShip + '/Indizi.js');
-      })
-      router.get(`/LogicGames/${user}/BattleShip/Interaction`, (req, res) => {
-        res.sendFile(path_to_BattleShip + '/Interaction.js');
-      })
-      router.get(`/LogicGames/${user}/BattleShip/Navi`, (req, res) => {
-        res.sendFile(path_to_BattleShip + '/Navi.js');
-      })
-
-      app.post(`/LogicGames/${user}/select`, (req, res) => {
-        res.json({
-          user: user,
-          status: 'success'
-        })
-      })
-
-      response.json({
-        status: 'success',
-        user: user
-      })
-
-      //skyscrapers
-      app.post(`/LogicGames/${user}/SkyScrapers/data`, (req, res) => {
-
-        db.update({ user: user }, {
-          $set: {
-            "skyscrapers.grid": req.body.grid,
-            "skyscrapers.cluegrid": req.body.cluegrid,
-            "skyscrapers.solution": req.body.solution,
-            "skyscrapers.usage": req.body.usage
-          }
-        }, {}, function () {
-        })
-        
-        console.log(user + ' sended his data about skyscrapers');
-
-        db.find({ user: user }, (err, docs) => {
-          user_data = docs[0];
-        })
-
-        res.json({
-          status: 'done'
-        })
-      })
-
-      app.post(`/LogicGames/${user}/SkyScrapers/get_data`, (req, res) => {
-
-        if (user_data.skyscrapers) { 
-          const grid = user_data.skyscrapers.grid;
-          const cluegrid = user_data.skyscrapers.cluegrid;
-          const solution = user_data.skyscrapers.solution;
-          const usage = user_data.skyscrapers.usage;
-          res.json({
-            status: 'done',
-            grid: grid,
-            cluegrid: cluegrid,
-            solution: solution,
-            usage: usage
-
-          })
-        }
-        else {
-          res.json({
-            status: 'empty'
-          })          
-        }
-        
-      })
-
-      //battleship
-      app.post(`/LogicGames/${user}/BattleShip/data`, (req, res) => {
-
-        db.update({ user: user }, {
-          $set: {
-            "battleship.grid": req.body.grid,
-            "battleship.naviPosizionate": req.body.naviPosizionate,
-            "battleship.elements": req.body.elements
-          }
-        }, {}, function () {
-        })
-
-        db.find({ user: user }, (err, docs) => {
-          user_data = docs[0];
-        })
-
-        console.log(user + ' sended his data about battleship');
-
-        res.json({
-          status: 'done'
-        })
-      })
-
-      app.post(`/LogicGames/${user}/BattleShip/get_data`, (req, res) => {
-
-        if (user_data.battleship) {
-          const grid = user_data.battleship.grid
-          const naviPosizionate = user_data.battleship.naviPosizionate
-          const elements = user_data.battleship.elements
-          res.json({
-            status: 'done',
-            grid: grid,
-            naviPosizionate: naviPosizionate,
-            elements: elements
-          })
-        }
-        else {
-          res.json({
-            status: 'empty'
-          })
-        }
-
-      })
-
-
-    }
-    else {
-      console.log('not registered');
-      response.json({
-        status: 'failed',
-      })
-    }
+  router.get(`/LogicGames/${user}`, (req, res) => {
+    res.sendFile(__dirname + '/LogicGames/index.html');
   });
+
+  //skyscrapers
+  router.get(`/LogicGames/${user}/SkyScrapers`, (req, res) => {
+    res.sendFile(path_to_skyscrapers + '/index.html');
+  })
+
+  router.get(`/LogicGames/${user}/SkyScrapers/Clues`, (req, res) => {
+    res.sendFile(path_to_skyscrapers + '/Clues.js');
+  })
+  router.get(`/LogicGames/${user}/SkyScrapers/Grid`, (req, res) => {
+    res.sendFile(path_to_skyscrapers + '/Grid.js');
+  })
+  router.get(`/LogicGames/${user}/SkyScrapers/GameCreation`, (req, res) => {
+    res.sendFile(path_to_skyscrapers + '/GameCreation.js');
+  })
+  router.get(`/LogicGames/${user}/SkyScrapers/Interaction`, (req, res) => {
+    res.sendFile(path_to_skyscrapers + '/Interaction.js');
+  })
+
+  //battleship
+  router.get(`/LogicGames/${user}/BattleShip`, (req, res) => {
+    res.sendFile(path_to_BattleShip + '/index.html');
+  })
+
+  router.get(`/LogicGames/${user}/BattleShip/Elements`, (req, res) => {
+    res.sendFile(path_to_BattleShip + '/Elements.js');
+  })
+  router.get(`/LogicGames/${user}/BattleShip/Grid`, (req, res) => {
+    res.sendFile(path_to_BattleShip + '/Grid.js');
+  })
+  router.get(`/LogicGames/${user}/BattleShip/Indizi`, (req, res) => {
+    res.sendFile(path_to_BattleShip + '/Indizi.js');
+  })
+  router.get(`/LogicGames/${user}/BattleShip/Interaction`, (req, res) => {
+    res.sendFile(path_to_BattleShip + '/Interaction.js');
+  })
+  router.get(`/LogicGames/${user}/BattleShip/Navi`, (req, res) => {
+    res.sendFile(path_to_BattleShip + '/Navi.js');
+  })
+
+  app.post(`/LogicGames/${user}/select`, (req, res) => {
+    res.json({
+      user: user,
+      status: 'success'
+    })
+  })
+
+  response.json({
+    status: 'success',
+    user: user
+  })
 })
 
 app.listen(port, () => console.log('listening to port ' + port));
-// const httpsServer = https.createServer({
-//   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//   cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-// }, app)
-
-//httpsServer.listen(3443, () => console.log('Secure server with https on port 3000'))
-
-// //TO REMOVE ELEMENTS FROM THE DATABASE
-// db.remove({ user: 'franco' }, { multi: false }, (err, numRemoved) => {
-//     console.log(numRemoved);
-// });
-
 
